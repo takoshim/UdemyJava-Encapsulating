729eb1272698be2d02dfd1b233103bd6f3663c32
tako2039 <takeo2039@gmail.com>
2022-04-08 15:18:10 -0500

    implement functionalities refill and print

--- Main.java
+++ Main.java
@@ -3,6 +3,13 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Printer printer = new Printer();
+        printer.printDocs(10);
+        printer.printDocs(15);
+        printer.refillToner(80);
+        printer.refillToner(120);
+        printer.refillToner(-10);
+        printer.refillToner(100);
+        printer.printDocs(120);
     }
 }
--- Printer.java
+++ Printer.java
@@ -1,12 +1,57 @@
 package com.timbuchalka;
 
 public class Printer {
-    private int tonerLevel = 20;
+    private int tonerLevel;
     private int numberOfPagePrinted;
     private boolean duplexPrintable = false;
 
-    public Printer(int tonerLevel, int numberOfPagePrinted,
+    private Printer(int tonerLevel, int numberOfPagePrinted,
                    boolean duplexPrintable) {
+        this.tonerLevel = tonerLevel;
+        this.numberOfPagePrinted = numberOfPagePrinted;
+        this.duplexPrintable = duplexPrintable;
+    }
+
+    public Printer() {
+        this(20, 0, false);
+        System.out.println("You bought a new printer!");
+        System.out.println("********************");
+    }
+
+    public void printDocs(int numberOfPrinting) {
+        int printCounter = 0;
+        if (numberOfPrinting < 0) {
+            System.out.println("Invalid number of page.");
+        }
+
+        System.out.println("Start printing " + numberOfPrinting + " page document.");
+        for (int i=1; i<=numberOfPrinting; ++i) {
+            if (tonerLevel <= 0) {
+                System.out.println("Not enough toner. Print stopped.");
+                break;
+            } else {
+                --tonerLevel;
+                ++numberOfPagePrinted;
+                ++printCounter;
+                System.out.println("  Page " + i + " printed.");
+            }
+        }
+        if (printCounter >= numberOfPrinting) {
+            System.out.println("Complete printing " + numberOfPrinting + " pages document.");
+        }
+        System.out.println("Toner level: " + this.tonerLevel);
+        System.out.println("********************");
+    }
+
+    public void turnOnDuplexPrint() {
+        this.duplexPrintable = true;
+        System.out.println("Duplex print feature on.");
+        System.out.println("********************");
+    }
+
+    public void refillToner(int tonerLevel) {
+        System.out.println("Refilling toner with " + tonerLevel + "% toner.");
+
         if (tonerLevel < 0 || tonerLevel > 100) {
             System.out.println("Invalid level of remaining toner.");
         } else if (tonerLevel < 100) {
@@ -15,17 +60,7 @@ public class Printer {
             System.out.println("A new genuine toner is set.");
             this.tonerLevel = tonerLevel;
         }
-
-        this.numberOfPagePrinted = numberOfPagePrinted;
-        this.duplexPrintable = duplexPrintable;
-    }
-
-    public void printDocs(int numberOfPrinting) {
-        if (numberOfPrinting < 0) {
-            System.out.println("Invalid number of page.");
-        }
-        this.numberOfPagePrinted += numberOfPrinting;
-        System.out.println("Printed: " + numberOfPagePrinted + " pages of document.");
-        }
+        System.out.println("Toner level: " + this.tonerLevel);
+        System.out.println("********************");
     }
 }
